86a2cdfcd57d92882ef812b4da186732e8e39450
SuperCoder <propscode254@gmail.com>
2024-01-12 11:09:05 +0100

    add persons

--- src/views/Persons.vue
+++ src/views/Persons.vue
@@ -1,32 +1,48 @@
-<!-- Persons.vue -->
 <template>
-    <div>
-      <h2 class="text-2xl font-bold mb-4">GOT Persons</h2>
-      <ul>
-        <li v-for="person in persons" :key="person.id" @click="showDetails(person.id)">
-          {{ person.name }} ({{ person.house.name }})
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Persons',
-    data() {
-      return {
-        persons: [],
-      };
+  <div>
+    <h2 class="text-2xl font-bold mb-4">GOT Persons</h2>
+    <ul>
+      <li v-for="person in persons" :key="person.slug">
+        <details>
+          <summary @click="loadPersons(person.slug)">{{ person.name }}</summary>
+          <div v-if="person.house">
+            <p>House: {{ person.house.name }}</p>
+          </div>
+          <ul v-if="person.quotes && person.quotes.length">
+            <li v-for="quote in person.quotes" :key="quote">
+              {{ quote }}
+            </li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Persons',
+  data() {
+    return {
+      persons: [],
+    };
+  },
+  created() {
+    this.loadPersons();
+  },
+  methods: {
+    async loadPersons() {
+      try {
+        const response = await axios.get('/api/v1/characters');
+        this.persons = response.data;
+        console.log(response.data);
+        
+      } catch (error) {
+        console.error('Error to load persons:', error);
+      }
     },
-    created() {
-      // Lógica para obtener las personas de la API y almacenarlas en this.persons
-      // (puedes usar axios o fetch)
-    },
-    methods: {
-      showDetails(personId) {
-        // Lógica para mostrar los detalles de una persona específica
-        // Puedes utilizar router.push para navegar a la subpágina correspondiente
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
